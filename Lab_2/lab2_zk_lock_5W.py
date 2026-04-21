c42d28af32161a2a7ceb6367d0f19167f04104e5
Edy Rahman <edhyra@student.ub.ac.id>
2026-04-21 20:28:16 +0700

    Create lab2_zk_lock_5W.py

---
+++ Lab_2/lab2_zk_lock_5W.py
@@ -0,0 +1,20 @@
+# lab2_zk_lock_5W.py
+from kazoo.client import KazooClient
+from kazoo.recipe.lock import Lock
+import threading, time
+
+def worker(worker_id):
+    zk = KazooClient(hosts='localhost:2181')
+    zk.start()
+    lock = Lock(zk, "/myapp/lock")
+    print(f"[Worker-{worker_id}] Mencoba acquire lock...")
+    with lock:
+        print(f"[Worker-{worker_id}] Lock diperoleh! Masuk CR.")
+        time.sleep(2)
+        print(f"[Worker-{worker_id}] Selesai, release lock.")
+    zk.stop()
+
+threads = [threading.Thread(target=worker, args=(i,)) for i in range(5)]
+for t in threads: t.start()
+for t in threads: t.join()
+print("Semua worker selesai!")
