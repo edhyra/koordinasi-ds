3930dbe9ef1776d1124b6b58a88bf2441caf2b65
Edy Rahman <edhyra@student.ub.ac.id>
2026-04-21 20:40:32 +0700

    Update zk lock handling with crash simulation
    Refactor lock acquisition to handle exceptions and simulate crash.

---
+++ Lab_2/lab2_zk_lock_W1crash.py
@@ -0,0 +1,26 @@
+# lab2_zk_lock_W1crash.py
+from kazoo.client import KazooClient
+from kazoo.recipe.lock import Lock
+import threading, time
+
+def worker(worker_id):
+    zk = KazooClient(hosts='localhost:2181')
+    zk.start()
+    lock = Lock(zk, "/myapp/lock")
+    print(f"[Worker-{worker_id}] Mencoba acquire lock...")
+    try:
+        with lock:
+            print(f"[Worker-{worker_id}] Masuk CR")
+            if worker_id == 1:
+                print(f"[Worker-{worker_id}] CRASH!")
+                raise Exception("Simulasi crash")
+            time.sleep(2)
+            print(f"[Worker-{worker_id}] Selesai")
+    except Exception as e:
+        print(f"[Worker-{worker_id}] Exception: {e}")
+    zk.stop()
+
+threads = [threading.Thread(target=worker, args=(i,)) for i in range(3)]
+for t in threads: t.start()
+for t in threads: t.join()
+print("Semua worker selesai!")
